6e99ad7c3d9a2f6f0c433d80f15417b128334b97
VTramon <vitorr.ooliveira@gmail.com>
2021-11-20 22:04:38 -0300

    changed some minor issues to make it work with the new routes of back-end

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -36,7 +36,7 @@ export function AuthProvider(props: AutheProvider) {
   const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=520ffc7ef3c0a245f581`;
 
   async function signIn(githubCode: string) {
-    const response = await api.post<AutheResponse>('authenticate', {
+    const response = await api.post<AutheResponse>('authenticate/web', {
       code: githubCode,
     });
 
@@ -60,7 +60,7 @@ export function AuthProvider(props: AutheProvider) {
     if (token) {
       api.defaults.headers.common.authorization = `Bearer ${token}`;
 
-      api.get<User>('profile').then(response => {
+      api.get<User>('profile').then((response) => {
         setUser(response.data);
       });
     }
